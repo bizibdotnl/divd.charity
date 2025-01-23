1cee5d98cceea3720f10fef8fa8f2bbc0da5a55b
Victor Gevers <322518+cookiemonster@users.noreply.github.com>
2025-01-23 14:57:37 +0100

    Create info.php

---
+++ info.php
@@ -0,0 +1,6 @@
+<html>
+<body>
+  <iframe src="https://giphy.com/embed/131bQid3vESGFW" width="480" height="259" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/131bQid3vESGFW">via GIPHY</a></p>  
+</body>
+</html>
+
